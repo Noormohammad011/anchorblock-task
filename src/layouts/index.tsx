fc14b710b0661f3cacf501b4b540dc22c4718a39
Noormohammad011 <noormohammad.net1503011@gmail.com>
2023-12-18 23:36:41 +0600

    adding reusable components

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/shared/Navbar";
 
 
 const Layout = () => {
   return (
     <div>
       <header>
-        {/* <Navbar /> */}
+        <Navbar />
       </header>
       <main>
         <Outlet />
